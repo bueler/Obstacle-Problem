5aae7e6ff87fb5a327a1ae40c99b164e28ccdbd8
Ed Bueler <elbueler@alaska.edu>
2017-10-15 09:58:25 -0800

    document memory error; note this print syntax is correct for both python2 and python3 ... I should use it generally ...

--- fmgexample.py
+++ fmgexample.py
@@ -4,16 +4,17 @@ import numpy as np
 
 f = lambda x,y: -8.0*(np.pi**2)*np.sin(2.0*np.pi*x)*np.sin(2.0*np.pi*y)
 
-minm = 2
-#numcycles = 6
+minm = 1
 numcycles = 6
+#numcycles = 7  # CAUSES MemoryError in Poisson2D() when attempts to allocate dense NxN matrix with m+2=257 and N=66049
 
 # generate fine grid and discretize Poisson on it
 m = minm
 for i in range(0, numcycles):
     m = 2 * m + 1
-print(m)
 N = (m + 2)**2
+print('%d x %d grid with N=%d total unknowns' % (m+2,m+2,N))
+
 A, U, F, _, X = Poisson2D(m, f, bvals = True)
 
 # get fmg solution
@@ -26,5 +27,6 @@ for j in range(0, m + 2):
     for i in range(0, m + 2):
         k = kk(i,j)
         Uexact[k] = uexact(X[i],X[j])
+
 print(np.linalg.norm(U - Uexact, np.inf))
 
